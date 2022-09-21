4f1e5f7e227abc3e80529b023f7e508bac8f32b0
rob-grokism <robert@grokism.com>
2022-09-21 15:26:07 -0500

    initial

--- Amazon_Simple_Storage_Service/Getting_Data_Into_S3/boto3/boto3actions.py
+++ Amazon_Simple_Storage_Service/Getting_Data_Into_S3/boto3/boto3actions.py
@@ -10,7 +10,7 @@ putMessage = b'Hi! I came from Boto3!'
 # put_object
 response = s3Client.put_object(
     Body = putMessage,
-    Bucket = 'das-demos',
+    Bucket = 'lu-misc',
     Key = 'boto3put.txt'
 )
 
@@ -18,16 +18,16 @@ print(response)
 
 # copy
 toCopy = {
-    'Bucket': 'das-demos',
+    'Bucket': 'lu-misc',
     'Key': 'boto3put.txt'
 }
 
-s3Resource.meta.client.copy(toCopy, 'das-demos', 'boto3copy.txt')
+s3Resource.meta.client.copy(toCopy, 'lu-misc', 'boto3copy.txt')
 
 # copy_object
 response = s3Client.copy_object(
-    Bucket = 'das-demos',
-    CopySource = '/das-demos/boto3put.txt',
+    Bucket = 'lu-misc',
+    CopySource = '/lu-misc/boto3put.txt',
     Key = 'boto3copyobject.txt'
 )
 
@@ -36,20 +36,19 @@ print(response)
 # upload_file
 boto3Upload = 'boto3upload.txt'
 
-s3Resource.meta.client.upload_file(boto3Upload, 'das-demos', boto3Upload)
+s3Resource.meta.client.upload_file(boto3Upload, 'lu-misc', boto3Upload)
 
 # upload_fileobj
 with open(boto3Upload, 'rb') as fileObj:
-    response = s3Client.upload_fileobj(fileObj, 'das-demos', 'boto3uploadobj.txt')
+    response = s3Client.upload_fileobj(fileObj, 'lu-misc', 'boto3uploadobj.txt')
     print(response)
 
-'''
+
 response = s3Client.put_bucket_accelerate_configuration(
-    Bucket='das-demos',
+    Bucket='lu-misc',
     AccelerateConfiguration={
-        'Status': 'Enabled'
+        'Status': 'Disabled'
     }
 )
 
 print(response)
-'''
